0267058a6ca7d5f07aa0a24f9fa6afde763711fa
marcelojacint <marcelojacintoferreirq@gmail.com>
2024-12-20 21:21:00 -0300

    init: projeto inicializado

---
+++ src/test/java/com/salaoBelezaPura/BelezaPuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salaoBelezaPura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelezaPuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
